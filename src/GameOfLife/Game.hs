b2c5c8fc9dc0c8cb7b57fe1a4d3ee158840e02de
Daniel Schierbeck <dasch@zendesk.com>
2014-05-04 20:26:40 +0000

    Add more gliders!

--- src/GameOfLife/Game.hs
+++ src/GameOfLife/Game.hs
@@ -32,6 +32,9 @@ main =
     let patterns = [
             (blinker, 2, 2),
             (glider, 8, 10),
+            (glider, 8, 50),
+            (glider, 8, 60),
+            (glider, 8, 70),
             (toad, 12, 20),
             (beacon, 6, 30),
             (acorn, 30, 50)]
