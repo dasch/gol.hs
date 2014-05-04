214b5df00897985af902f640f3b3434b4fec35d1
Daniel Schierbeck <dasch@zendesk.com>
2014-05-04 17:01:59 +0000

    Convert all the patterns to parses

--- src/GameOfLife/Game.hs
+++ src/GameOfLife/Game.hs
@@ -28,6 +28,10 @@ printWorld world = do
     print world
     threadDelay 400000
 
+addPattern :: ([Cell], Int, Int) -> [Cell]
+addPattern (pattern, y, x) = map (moveCell y x) pattern
+
 main =
-    let world = World $ blinker 2 2 ++ glider 8 10 ++ toad 12 20 ++ beacon 6 30
+    let patterns = [(blinker, 2, 2), (glider, 8, 10), (toad, 12, 20), (beacon, 6, 30)]
+        world = World $ concat $ map addPattern patterns
     in mapM printWorld $ evolutions world
--- src/GameOfLife/Patterns.hs
+++ src/GameOfLife/Patterns.hs
@@ -1,24 +1,17 @@
 module GameOfLife.Patterns where
 
-import GameOfLife.World
 import GameOfLife.Parser
 
--- y y y
-blinker :: Int -> Int -> [Cell]
-blinker y x = [Cell y x, Cell y (x + 1), Cell y (x + 2)]
+blinker = parsePattern ["@ @ @"]
 
--- . y .
--- . . y
--- y y y
-glider y x = [Cell y (x + 1), Cell (y + 1) (x + 2), Cell (y + 2) x, Cell (y + 2) (x + 1), Cell (y + 2) (x + 2)]
+glider = parsePattern [". @ .",
+                       ". . @",
+                       "@ @ @"]
 
--- . y y y
--- y y y .
-toad y x = [Cell y (x + 1), Cell y (x + 2), Cell y (x + 3), Cell (y + 1) x, Cell (y + 1) (x + 1), Cell (y + 1) (x + 2)]
+toad = parsePattern [". @ @ @",
+                     "@ @ @ ."]
 
-beacon y x =
-    let pattern = ["@ @ . .",
-                   "@ @ . .",
-                   ". . @ @",
-                   ". . @ @"]
-    in map (moveCell y x) $ parsePattern pattern
+beacon = parsePattern ["@ @ . .",
+                       "@ @ . .",
+                       ". . @ @",
+                       ". . @ @"]
