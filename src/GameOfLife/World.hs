e0e230980f5c72d6fe46a337a26943a6e7d51186
Daniel Schierbeck <dasch@zendesk.com>
2014-05-07 13:44:38 +0000

    Use a Set to represent the collection of cells in the world

--- src/GameOfLife/World.hs
+++ src/GameOfLife/World.hs
@@ -10,68 +10,69 @@ module GameOfLife.World (
 ) where
 
 import Data.List
+import qualified Data.Set as Set
 import Control.Monad
 
 -- A cell is represented by its location in the world.
 data Cell = Cell Int Int deriving (Show, Eq)
 
 -- The world is represented by a list of live cells.
-data World = World [Cell]
+type World = Set.Set Cell
+
+instance Ord Cell where
+    Cell a b `compare` Cell a' b' = (a, b) `compare` (a', b')
 
 newWorld :: World
-newWorld = World []
+newWorld = Set.empty
 
-worldWithCells = World
+worldWithCells :: [Cell] -> World
+worldWithCells = Set.fromList
 
-cellsIn (World cells) = cells
+cellsIn :: World -> [Cell]
+cellsIn = Set.toList
 
-cell `inWorld` World liveCells = cell `elem` liveCells
+inWorld :: Cell -> World -> Bool
+inWorld = Set.member
 
 -- Evolves the world a single step.
 --
 -- Takes the live cells that survives the round and adds the cells that sprung
 -- to life.
 tick :: World -> World
-tick (World cells) = World $ nub $ survivingCells cells ++ resurrectCells cells
+tick world = survivingCells world `Set.union` resurrectCells world
 
 -- An infinite list of worlds, each being the evolution of the one before it.
 evolutions :: World -> [World]
 evolutions = iterate tick
 
 cellCount :: World -> Int
-cellCount (World liveCells) = length liveCells
+cellCount = Set.size
 
-survivingCells :: [Cell] -> [Cell]
-survivingCells liveCells = filter (canSurvive liveCells) liveCells
+survivingCells :: World -> World
+survivingCells world = Set.filter (canSurvive world) world
 
-resurrectCells :: [Cell] -> [Cell]
-resurrectCells cells =
-    let candidateCells = nub . concat $ map neighboringCells cells
-    in filter (canBeRessurected cells) candidateCells
+resurrectCells :: World -> World
+resurrectCells world =
+    let candidateCells = Set.unions $ map neighboringCells $ cellsIn world
+    in Set.filter (canBeRessurected world) candidateCells
 
 -- Figures out whether the cell can survive.
-canSurvive :: [Cell] -> Cell -> Bool
-canSurvive liveCells cell =
-    -- We count the live neighbors, but only consider the first four, since we
-    -- really only care whether the count is either two or three.
-    let count = length $ take 4 $ liveNeighbors cell liveCells
-    in count `elem` [2, 3]
+canSurvive :: World -> Cell -> Bool
+canSurvive world cell = neighborCount world cell `elem` [2, 3]
 
 -- Figures out whether the cell can be resurrected.
-canBeRessurected :: [Cell] -> Cell -> Bool
-canBeRessurected liveCells cell =
-    -- We count the live neighbors, but only consider the first four, since we
-    -- really only care whether the count is eyactly three.
-    let count = length $ take 4 $ liveNeighbors cell liveCells
-    in count == 3
+canBeRessurected :: World -> Cell -> Bool
+canBeRessurected world cell = neighborCount world cell == 3
+
+neighborCount world cell = Set.size $ liveNeighbors cell world 
 
 -- For a given cell and set of live cells, finds the subset of the cell's
 -- neighbors that are alive.
-liveNeighbors :: Cell -> [Cell] -> [Cell]
-liveNeighbors cell = intersect $ neighboringCells cell
+liveNeighbors :: Cell -> World -> World
+liveNeighbors cell = Set.intersection $ neighboringCells cell
 
-neighboringCells :: Cell -> [Cell]
-neighboringCells (Cell y x) = do
+neighboringCells :: Cell -> World
+neighboringCells (Cell y x) = Set.fromList $ do
     y' <- [y - 1, y, y + 1]
     x' <- [x - 1, x, x + 1]
     guard $ (y, x) /= (y', x') -- the cell is not a neighbor of itself.
